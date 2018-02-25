31d145dc0b16443268cebe23d40aa9a00d1625fd
Daniel Scheffknecht <daniel.scheffkn@gmail.com>
2018-02-25 10:15:48 +0100

    Disable for Android, as it crashes otherwise

--- index.js
+++ index.js
@@ -9,56 +9,58 @@ var listener = null;
 const isDroid = Platform.OS=='android';
 const isIOS = Platform.OS=='ios';
 
-subscription = gpsStateEmitter.addListener('OnStatusChange', (response)=>{
-	if(listener){
-		var status = null;
+if (isIOS) {
+	subscription = gpsStateEmitter.addListener('OnStatusChange', (response)=>{
+		if(listener){
+			var status = null;
+			if(isIOS){
+				status = response;
+			}else{
+				status = response.status;
+			}
+
+			if(status)
+				listener.apply(this, [status]);
+		}
+	});
+
+	GPSState.addListener = (callback)=>{
+		if(typeof callback == 'function'){
+			listener = callback;
+			GPSState._startListen();
+		}
+	}
+
+	GPSState.removeListener = (callback)=>{
+		if(subscription){
+			GPSState._stopListen();
+			subscription.remove();
+			subscription = null;
+		}
+	}
+
+	GPSState.getStatus = ()=>{
+		return GPSState._getStatus();
+	}
+
+	GPSState.openSettings = ()=>{
+		GPSState._openSettings();
+	}
+
+	GPSState.requestAuthorization = (authType)=>{
 		if(isIOS){
-			status = response;
+			var type = parseInt(authType);
+			var min = GPSState.STATUS_NOT_DETERMINED;
+			var max = GPSState.STATUS_AUTHORIZED_WHENINUSE;
+			var inRange = (type>=min && type <= max);
+
+			if(isNaN(type) || !inRange){
+				type = GPSState.AUTHORIZED_WHENINUSE;
+			}
+			GPSState._requestAuthorization(type);
 		}else{
-			status = response.status;
+			GPSState._requestAuthorization();
 		}
-
-		if(status)
-			listener.apply(this, [status]);
-	}
-});
-
-GPSState.addListener = (callback)=>{
-	if(typeof callback == 'function'){
-		listener = callback;
-		GPSState._startListen();
-	}
-}
-
-GPSState.removeListener = (callback)=>{
-	if(subscription){
-		GPSState._stopListen();
-		subscription.remove();
-		subscription = null;
-	}
-}
-
-GPSState.getStatus = ()=>{
-	return GPSState._getStatus();
-}
-
-GPSState.openSettings = ()=>{
-	GPSState._openSettings();
-}
-
-GPSState.requestAuthorization = (authType)=>{
-	if(isIOS){
-		var type = parseInt(authType);
-		var min = GPSState.STATUS_NOT_DETERMINED;
-		var max = GPSState.STATUS_AUTHORIZED_WHENINUSE;
-		var inRange = (type>=min && type <= max);
-
-		if(isNaN(type) || !inRange){
-			type = GPSState.AUTHORIZED_WHENINUSE;
-		}
-		GPSState._requestAuthorization(type);
-	}else{
-		GPSState._requestAuthorization();
 	}
 }
 
